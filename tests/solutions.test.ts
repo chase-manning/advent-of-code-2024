5b5ac3ae10980d140304e2e780debfb5d7365eb4
Chase Manning <chase@manning.dev>
2024-12-02 10:44:50 +0000

    ✅ add tests for solutions

---
+++ tests/solutions.test.ts
@@ -0,0 +1,10 @@
+import { part1 } from "../solutions/01";
+import { part2 } from "../solutions/01";
+
+test("Day 01 Part 01", () => {
+  expect(part1()).toBe(2375403);
+});
+
+test("Day 01 Part 02", () => {
+  expect(part2()).toBe(23082277);
+});
